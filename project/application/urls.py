50a99487ee196ae1b9a004d04defb17afc1dc2df
Alexander Sender <141014024+SenderAlex@users.noreply.github.com>
2025-03-16 22:32:30 +0300

    finished

--- project/application/urls.py
+++ project/application/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index),
-    path('data/', views.data),
-    path('test/', views.test)
-
+    path('', views.index, name='index'),
+    path('laliga/', views.laliga, name='laliga'),
+    path('premiereleague/', views.premiereleague, name='premiereleague'),
+    path('seriea/', views.seriea, name='seriea'),
+    path('bundesliga/', views.bundesliga, name='bundesliga'),
+    path('liga1/', views.liga1, name='liga1')
 ]
--- project/application/views.py
+++ project/application/views.py
@@ -3,12 +3,22 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h1>Добро пожаловать в Django</h1>")
+    return render(request, "application/index.html")
 
 
-def data(request):
-    return HttpResponse("<h1>Вы зашли на страницу Django Data</h1>")
+def laliga(request):
+    return render(request, "application/laliga.html")
 
 
-def test(request):
-    return HttpResponse("<h1>Вы на странице Django Test</h1>")
+def premiereleague(request):
+    return render(request, "application/premiereleague.html")
+
+def seriea(request):
+    return render(request, "application/seriea.html")
+
+
+def bundesliga(request):
+    return render(request, "application/bundesliga.html")
+
+def liga1(request):
+    return render(request, "application/liga1.html")
